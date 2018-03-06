965168ee1f6bc0f7b14e347506fb2f7e4b955ce7
Sienna <sienna.m.wood@gmail.com>
2018-03-06 08:52:10 -0700

    snake body and snake history as queues
    Co-authored-by: Sienna M. Wood <sienna.m.wood@gmail.com>
    Co-authored-by: Grant Reed <grant.davis.reed@gmail.com>

--- client/src/redux/snakeReducer.js
+++ client/src/redux/snakeReducer.js
@@ -1,17 +1,39 @@
 import * as actionTypes from './actionTypes';
-import * as helpers from './snakeHelpers';
+import * as snakeHelpers from './snakeHelpers';
 
 const defaultState = {
   0: {
     direction: 'left',
     status: 'alive',
-    position: [
+    body: [ // queue
       { row: 5, column: 5 },
       { row: 5, column: 6 },
       { row: 4, column: 6 },
       { row: 4, column: 7 },
     ],
+    history: [ // queue
+      { row: 4, column: 8 },
+      { row: 4, column: 9 },
+      { row: 4, column: 10 },
+      { row: 4, column: 11 },
+    ],
   },
+  // 1: {
+  //   direction: 'right',
+  //   status: 'alive',
+  //   body: [ // queue
+  //     { row: 5, column: 10 },
+  //     { row: 5, column: 9 },
+  //     { row: 5, column: 8 },
+  //     { row: 5, column: 7 },
+  //   ],
+  //   history: [ // queue
+  //     { row: 4, column: 7 },
+  //     { row: 3, column: 7 },
+  //     { row: 2, column: 7 },
+  //     { row: 1, column: 7 },
+  //   ],
+  // }
 };
 
 export default function snakesReducer(state = defaultState, action) {
