9041026f2c04e5ab961740a3a42ce7264151ef98
Sienna <sienna.m.wood@gmail.com>
2018-03-06 10:00:37 -0700

    validate snake direction changes

--- client/src/redux/snakeHelpers.js
+++ client/src/redux/snakeHelpers.js
@@ -1,1 +1,18 @@
-// helpers for snake operations
+export const validateDirectionChange = (oldDir, newDir) => {
+  switch (oldDir) {
+    case 'down':
+    case 'up':
+      if (newDir === 'left' || newDir === 'right') {
+        return true;
+      }
+      break;
+    case 'left':
+    case 'right':
+    default:
+      if (newDir === 'up' || newDir === 'down') {
+        return true;
+      }
+  }
+
+  return false;
+};
