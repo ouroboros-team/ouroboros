b0e7ee68dd4669572053733b99e68140300708c2
Grant Reed <grant.davis.reed@gmail.com>
2018-03-08 09:40:01 -0800

    add route to App to capture peerID from URL
    Co-authored-by: Sienna M. Wood <sienna.m.wood@gmail.com>
    Co-authored-by: Grant Reed <grant.davis.reed@gmail.com>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import * as actionCreators from '../redux/actionCreators';
 import '../assets/styles/index.scss';
 
 import Header from './Header';
 import Game from './Game';
 // TODO: routes for informational screens
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Game />
-    </div>
-  );
-};
+const App = props => (
+  <div>
+    <Header />
+    <Route
+      path='/:peerId'
+      render={({ match }) => {
+        // save peerId to redux store
+        console.log(match.params.peerId);
+        props.p2pGetPeerIdFromURL(match.params.peerId);
+        return (
+          <Game />
+        );
+      }}
+    />
+  </div>
+);
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  p2pGetPeerIdFromURL: (id) => {
+    dispatch(actionCreators.p2pGetPeerIdFromURL(id));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(App);
