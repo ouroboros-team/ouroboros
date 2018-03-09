0d3c7716b12e6c39ee5a503c2dfd6150cbd21467
Sienna <sienna.m.wood@gmail.com>
2018-03-09 09:28:32 -0700

    playerList displays connected peers by localId

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-export default class PlayerList extends React.Component {
-  render() {
-    return (
-      <div id='player-list' className='three columns'>
-        <ul>
-          <li className='id-0'>Player 0</li>
-          <li className='id-1'>Player 1</li>
-          <li className='id-2'>Player 2</li>
-          <li className='id-3'>Player 3</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const PlayerList = (props) => {
+  const list = [];
+  let localId = '';
+  let playerName = '';
+  let className = '';
+  const peerIds = Object.keys(props.peers);
+
+  peerIds.forEach((peerId) => {
+    localId = props.peers[peerId].localId;
+    playerName = `Player ${localId}`;
+    className = `id-${localId}`;
+
+    list.push(<li key={localId} className={className}>{playerName}</li>);
+  });
+
+  return (
+    <div id='player-list'>
+      <ul>
+        <li className='label'>Connected Players</li>
+        {list}
+      </ul>
+    </div>
+  );
+};
+
+PlayerList.propTypes = {
+  peers: propTypes.object,
+};
+
+PlayerList.defaultProps = {
+  peers: {},
+};
+
+export default PlayerList;
