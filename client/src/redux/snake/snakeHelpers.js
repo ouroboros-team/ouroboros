98756ceb7bf3aafbe394f85cc5f7ad39df1d640c
Sienna <sienna.m.wood@gmail.com>
2018-03-09 12:40:45 -0700

    actions to incorporate new peer snake data
    Co-authored-by: Sienna M. Wood <sienna.m.wood@gmail.com>
    Co-authored-by: Grant Reed <grant.davis.reed@gmail.com>

--- client/src/redux/snake/snakeHelpers.js
+++ client/src/redux/snake/snakeHelpers.js
@@ -1,3 +1,5 @@
+import * as constants from '../../constants';
+
 export const validateDirectionChange = (oldDir, newDir) => {
   switch (oldDir) {
     case 'down':
@@ -16,3 +18,31 @@ export const validateDirectionChange = (oldDir, newDir) => {
 
   return false;
 };
+
+
+// data is assumed to be in the same format as snakes in the Redux store
+export const updateSnakeDataMutate = (newSnake, data) => {
+  newSnake.direction = data.direction;
+
+  const gap = Math.abs(newSnake.body[0].tu - data.body[0].tu);
+  let counter = gap;
+
+  while (counter > 0) {
+    // add new coordinates
+    newSnake.body.unshift(data.body[gap - 1]);
+
+    // move tail to history
+    newSnake.history.unshift(newSnake.body.pop());
+
+    // purge history
+    if (newSnake.history.length > constants.HISTORY_LENGTH) {
+      newSnake.history.length = constants.HISTORY_LENGTH;
+    }
+
+    counter -= 1;
+  }
+};
+
+export const getSnakeLength = (tu) => (
+  constants.INITIAL_SNAKE_LENGTH
+);
--- client/src/redux/snake/snakeReducer.js
+++ client/src/redux/snake/snakeReducer.js
@@ -1,6 +1,6 @@
 import * as actionTypes from '../actionTypes';
 import * as snakeHelpers from './snakeHelpers';
-import * as metaHelpers from '../metaHelpers';
+import * as helpers from '../metaHelpers';
 
 const defaultState = {
   0: {
@@ -19,22 +19,21 @@ const defaultState = {
       { row: 4, column: 11, tu: 0 },
     ],
   },
-  // 1: {
-  //   direction: 'right',
-  //   status: 'alive',
-  //   body: [ // queue
-  //     { row: 5, column: 10, tu: 7 },
-  //     { row: 5, column: 9, tu: 6 },
-  //     { row: 5, column: 8, tu: 5 },
-  //     { row: 5, column: 7, tu: 4 },
-  //   ],
-  //   history: [ // queue
-  //     { row: 4, column: 7, tu: 3 },
-  //     { row: 3, column: 7, tu: 2 },
-  //     { row: 2, column: 7, tu: 1 },
-  //     { row: 1, column: 7, tu: 0 },
-  //   ],
-  // }
+  1: {
+    direction: 'right',
+    status: 'alive',
+    body: [ // queue
+      { row: 0, column: 9, tu: 6 },
+      { row: 0, column: 8, tu: 5 },
+      { row: 0, column: 7, tu: 4 },
+      { row: 1, column: 7, tu: 3 },
+    ],
+    history: [ // queue
+      { row: 2, column: 7, tu: 2 },
+      { row: 3, column: 7, tu: 1 },
+      { row: 4, column: 7, tu: 0 },
+    ],
+  }
 };
 
 export default function snakesReducer(state = defaultState, action) {
@@ -44,9 +43,32 @@ export default function snakesReducer(state = defaultState, action) {
         return state;
       }
 
-      const newState = metaHelpers.deepClone(state);
+      const newState = helpers.deepClone(state);
       newState[action.id].direction = action.direction;
       return newState;
+    }
+    case actionTypes.UPDATE_PEER_SNAKE_DATA: {
+      const id = Number(action.id);
+
+      // if no snake data is held for this snake, simply add received data
+      if (!state[id]) {
+        const newState = helpers.deepClone(state);
+        newState[id] = action.data;
+        return newState;
+      }
+
+      const mostRecentTu = Number(state[id].body[0].tu);
+      const newDataTu = Number(action.data.body[0].tu);
+
+      // if data TU is more recent than most recent TU for this snake,
+      // incorporate new data
+      if (newDataTu > mostRecentTu) {
+        const newState = helpers.deepClone(state);
+        snakeHelpers.updateSnakeDataMutate(newState[id], action.data);
+        return newState;
+      }
+
+      return state;
     }
     default: {
       return state;
