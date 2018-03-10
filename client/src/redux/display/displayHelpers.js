34979b62f7857ab1cf370a9a85cfd8578f15cf5c
Sienna <sienna.m.wood@gmail.com>
2018-03-09 18:50:28 -0700

    factor out board aggregating and initial board functions
    Co-authored-by: Sienna M. Wood <sienna.m.wood@gmail.com>
    Co-authored-by: Grant Reed <grant.davis.reed@gmail.com>

--- client/src/redux/display/displayHelpers.js
+++ client/src/redux/display/displayHelpers.js
@@ -1,16 +1,13 @@
+import merge from 'lodash/merge';
+
 import store from '../store';
 import * as boardHelpers from '../board/boardHelpers';
+import * as snakeHelpers from '../snake/snakeHelpers';
 import * as helpers from '../metaHelpers';
 import * as constants from '../../constants';
 
-export const dequeueTailMutate = (snake) => {
-  const tail = snake.body.pop();
-  snake.history.unshift(tail);
-  return tail;
-};
-
 export const enqueueHeadMutate = (snake, nextPosition) => {
-  snake.body.unshift(nextPosition);
+  snake.positions.unshift(nextPosition);
   return nextPosition;
 };
 
@@ -45,39 +42,58 @@ export const calculateNextCoords = (direction, oldCoords) => {
   return { row, column, tu: oldCoords.tu + 1 };
 };
 
+export const aggregateBoards = (lastTu) => {
+  const clonedBoards = helpers.deepClone(store.getState().boards);
+  const length = snakeHelpers.getSnakeLength(lastTu);
+  let aggregatedBoard = {};
+
+  for (let i = (lastTu - length) + 1; i <= lastTu; i++) {
+    // newer data overwrites older data, no collision checking yet
+    if (clonedBoards[i]) {
+      aggregatedBoard = merge(aggregatedBoard, clonedBoards[i]);
+    }
+  }
+
+  return aggregatedBoard;
+};
+
+export const getInitialDisplayBoard = () => (
+  aggregateBoards(constants.INITIAL_TU)
+);
+
 export const getNextDisplayBoard = () => {
   const state = store.getState();
   const tu = state.info.tu;
+  const newBoard = aggregateBoards(tu);
 
-  // deep clone allows free mutation
-  const newBoard = helpers.deepClone(state.boards[tu - 1] || {});
+  // combine boards needed for current snake length - 1
+  // (predicted move will make full snake length)
+
   const snakesObj = helpers.deepClone(state.snakes);
-
   const snakeIds = Object.keys(snakesObj);
 
+  const length = snakeHelpers.getSnakeLength(tu);
+
   let snake;
-  let tail;
   let next;
 
+  // predict next moves, fill in missing data with predictions
   snakeIds.forEach((id) => {
     snake = snakesObj[id];
 
     // run once for all snakes, more for snakes with missing TUs
-    while (snake.body[0].tu <= tu) {
-      // remove tail from cloned snake object
-      tail = dequeueTailMutate(snake);
-
-      // remove tail from newBoard
-      boardHelpers.removeCoordinatesMutate(newBoard, tail);
-
+    while (snake.positions[0].tu <= tu) {
       // calculate next coordinates (predicted)
-      next = calculateNextCoords(snake.direction, snake.body[0]);
+      next = calculateNextCoords(snake.direction, snake.positions[0]);
 
       // add predicted coordinates to cloned snake object
       enqueueHeadMutate(snake, next);
 
-      // add next position to newBoard
-      boardHelpers.addCoordinatesMutate(newBoard, next, snake, id);
+      // add next position to newBoard if it is within range
+      // (based on target TU and snake length)
+      if (snake.positions[0].tu <= tu && snake.positions[0].tu > tu - length) {
+        boardHelpers.addCoordinatesMutate(newBoard, next, snake, id);
+      }
     }
   });
 
--- client/src/redux/display/displayReducer.js
+++ client/src/redux/display/displayReducer.js
@@ -1,17 +1,10 @@
-import store from '../store';
 import * as actionTypes from '../actionTypes';
-import * as helpers from '../metaHelpers';
 import * as displayHelpers from './displayHelpers';
-import * as constants from '../../constants';
 
 export default function displayReducer(state = {}, action) {
   switch (action.type) {
     case actionTypes.GET_INITIAL_DISPLAY_BOARD: {
-      const model = store.getState().boards[constants.INITIAL_TU] || {};
-      if (model){
-        return helpers.deepClone(model);
-      }
-      return state;
+      return displayHelpers.getInitialDisplayBoard();
     }
     case actionTypes.GET_NEXT_DISPLAY_BOARD: {
       return displayHelpers.getNextDisplayBoard();
