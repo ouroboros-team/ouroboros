93c0f1e6e5c998b66d61536d9dec34fd07921937
Sienna <sienna.m.wood@gmail.com>
2018-03-13 17:58:50 -0600

    test Header

--- client/src/components/test/Header.test.js
+++ client/src/components/test/Header.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../Header';
 
-it('renders without crashing', () => {
-  shallow(<Header />);
+describe('Header', () => {
+  it('shallow renders', () => {
+    shallow(<Header />);
+  });
+
+  it('should render an h1 and an img.logo', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img.logo').length).toEqual(1);
+    expect(wrapper.find('h1').length).toEqual(1);
+  });
 });
