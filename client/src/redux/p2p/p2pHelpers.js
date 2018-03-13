31ea5aaebfec55bc3c690e14b99c681253e0f9cc
Sienna M. Wood <siennamw@users.noreply.github.com>
2018-03-13 14:12:40 -0600

    Fix server errors (#22)
    * all get requests to server directed to
    
    * remove --production=false from heroku build script
    
    * roll back hash routing
    
    * add more STUN servers
    
    * change order of routes in server.js,
    trying to prevent peerjs requests from being routed to index.html

--- client/src/redux/p2p/p2pHelpers.js
+++ client/src/redux/p2p/p2pHelpers.js
@@ -12,6 +12,9 @@ export const initializeOwnPeerObject = () => {
     config: {
       iceServers: [
         { url: 'stun:stun1.l.google.com:19302' },
+        { url: 'stun:stun2.l.google.com:19302' },
+        { url: 'stun:stun3.l.google.com:19302' },
+        { url: 'stun:stun4.l.google.com:19302' },
         {
           url: 'turn:numb.viagenie.ca',
           credential: 'Asp8&Viper',
