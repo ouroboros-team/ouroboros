3cbf6a1201fb3c43b30af68fa12ccd428eb79523
Sienna <sienna.m.wood@gmail.com>
2018-03-13 12:10:57 -0600

    all get requests to server directed to

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const peer = require('peer');
+const path = require('path');
 
 const port = process.env.PORT || 3001;
 
@@ -8,7 +9,11 @@ app.set('port', port);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 const server = app.listen(app.get('port'));
