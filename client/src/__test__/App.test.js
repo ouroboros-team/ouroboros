799ef479b8ceadc411b19cf3e96b8c87f8cfa54e
Sienna M. Wood <siennamw@users.noreply.github.com>
2018-03-13 23:20:45 -0600

    First component tests (#23)
    * update root `.gitignore`
    
    * add test shortcut to root `package.json`
    
    * add libraries to mock react router and redux store
    
    * test App renders with both shallow and mount
    
    * test Header
    
    * move and rename test directory
    
    
    Co-authored-by: Sienna M. Wood <sienna.m.wood@gmail.com>
    Co-authored-by: Grant Reed <grant.davis.reed@gmail.com>

---
+++ client/src/__test__/App.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import MockRouter from 'react-mock-router';
+
+import App from '../components/App';
+
+const initialState = {};
+const mockStore = configureStore([ thunk ]);
+const store = mockStore(initialState);
+
+describe('App component', () => {
+  beforeEach(() => {
+  });
+
+  it('shallow renders', () => {
+    shallow(<App store={store} />);
+  });
+
+  it('mounts with router', () => {
+    mount(
+      <MockRouter>
+        <App store={store} />
+      </MockRouter>,
+    );
+  });
+});
