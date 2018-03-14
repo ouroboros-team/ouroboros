4e67a5ce52fb47772a6453de9b841d7e8e01d7d7
Sienna <sienna.m.wood@gmail.com>
2018-03-14 15:47:32 -0600

    remove calculateNextCoords from displayHelpers

--- client/src/redux/display/displayHelpers.js
+++ client/src/redux/display/displayHelpers.js
@@ -14,37 +14,6 @@ export const addCoordinatesMutate = (board, coords, snake, snakeId) => {
   };
 };
 
-export const calculateNextCoords = (direction, oldCoords) => {
-  let row = Number(oldCoords.row);
-  let column = Number(oldCoords.column);
-
-  switch (direction) {
-    case 'up':
-      row -= 1;
-      if (row < 0) {
-        row += constants.GRID_SIZE;
-      }
-      break;
-    case 'down':
-      row += 1;
-      row %= constants.GRID_SIZE;
-      break;
-    case 'left':
-      column -= 1;
-      if (column < 0) {
-        column += constants.GRID_SIZE;
-      }
-      break;
-    case 'right':
-    default:
-      column += 1;
-      column %= constants.GRID_SIZE;
-      break;
-  }
-
-  return { row, column, tu: oldCoords.tu + 1 };
-};
-
 export const aggregateBoards = (lastTu) => {
   const snakesObj = helpers.deepClone(store.getState().snakes);
   const snakeIds = Object.keys(snakesObj);
@@ -95,7 +64,7 @@ export const getNextDisplayBoard = () => {
     // run once for all snakes, more for snakes with missing TUs
     while (!snake.positions[tu + 1]) {
       // calculate next coordinates (predicted)
-      next = calculateNextCoords(snake.direction, snake.positions[mostRecentTu.toString()]);
+      next = snakeHelpers.calculateNextCoords(snake.direction, snake.positions[mostRecentTu.toString()]);
       mostRecentTu += 1;
 
       // add predicted coordinates to cloned snake object
