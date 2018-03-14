d8350e7cabedce82772a9a86fd51f47f1574cac7
Sienna <sienna.m.wood@gmail.com>
2018-03-14 11:26:38 -0600

    remove unneeded imports

--- client/src/redux/metaActionCreators.js
+++ client/src/redux/metaActionCreators.js
@@ -1,9 +1,6 @@
-import * as constants from '../constants';
-
 import * as boardActions from './board/boardActionCreators';
 import * as displayActions from './display/displayActionCreators';
 import * as infoActions from './info/infoActionCreators';
-import * as p2pActions from './p2p/p2pActionCreators';
 import * as snakeActions from './snake/snakeActionCreators';
 
 export const handleTuTick = id => (
