af751bbbf2e3510b32276c4e17c417b187e21431
Grant Reed <grant.davis.reed@gmail.com>
2018-03-16 16:39:32 -0700

    dead snakes not displayed

--- client/src/redux/display/displayHelpers.js
+++ client/src/redux/display/displayHelpers.js
@@ -22,12 +22,14 @@ export const aggregateBoards = (lastTu) => {
   let snake = {};
 
   snakeIds.forEach((id) => {
-    snake = snakesObj[id];
+    if (snakeHelpers.snakeIsAlive(id)) {
+      snake = snakesObj[id];
 
-    for (let i = (lastTu - length) + 1; i <= lastTu; i++) {
-      // newer data overwrites older data, no collision checking yet
-      if (snake.positions.byKey[i]) {
-        addCoordinatesMutate(aggregatedBoard, snake.positions.byKey[i], snake, id);
+      for (let i = (lastTu - length) + 1; i <= lastTu; i++) {
+        // newer data overwrites older data, no collision checking yet
+        if (snake.positions.byKey[i]) {
+          addCoordinatesMutate(aggregatedBoard, snake.positions.byKey[i], snake, id);
+        }
       }
     }
   });
@@ -58,24 +60,26 @@ export const getNextDisplayBoard = () => {
 
   // predict next moves, fill in missing data with predictions
   snakeIds.forEach((id) => {
-    snake = snakesObj[id];
-    mostRecentTu = Number(snake.positions.byIndex[0]);
+    if (snakeHelpers.snakeIsAlive(id)) {
+      snake = snakesObj[id];
+      mostRecentTu = Number(snake.positions.byIndex[0]);
 
-    // run once for all snakes, more for snakes with missing TUs
-    while (mostRecentTu < tu) {
-      // calculate next coordinates (predicted)
-      next = snakeHelpers.calculateNextCoords(snake.direction, snake.positions.byKey[`${mostRecentTu}`]);
+      // run once for all snakes, more for snakes with missing TUs
+      while (mostRecentTu < tu) {
+        // calculate next coordinates (predicted)
+        next = snakeHelpers.calculateNextCoords(snake.direction, snake.positions.byKey[`${mostRecentTu}`]);
 
-      mostRecentTu += 1;
+        mostRecentTu += 1;
 
-      // add predicted coordinates to cloned snake object
-      snake.positions.byKey[`${mostRecentTu}`] = next;
-      snake.positions.byIndex.unshift(`${mostRecentTu}`);
+        // add predicted coordinates to cloned snake object
+        snake.positions.byKey[`${mostRecentTu}`] = next;
+        snake.positions.byIndex.unshift(`${mostRecentTu}`);
 
-      // add next position to newBoard if it is within range
-      // (based on target TU and snake length)
-      if (mostRecentTu <= tu && mostRecentTu > tu - length) {
-        addCoordinatesMutate(newBoard, next, snake, id);
+        // add next position to newBoard if it is within range
+        // (based on target TU and snake length)
+        if (mostRecentTu <= tu && mostRecentTu > tu - length) {
+          addCoordinatesMutate(newBoard, next, snake, id);
+        }
       }
     }
   });
