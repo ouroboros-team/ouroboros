26fd17cf1ef1005b3335f944b64b0beb94b2fcd1
Sienna <sienna.m.wood@gmail.com>
2018-03-16 16:12:09 -0600

    GameBoard render test

---
+++ client/src/__test__/GameBoard.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import GameBoard from '../components/GameBoard';
+
+const initialState = { displayBoard: {} };
+const mockStore = configureStore([ thunk ]);
+const store = mockStore(initialState);
+
+describe('GameBoard component', () => {
+  beforeEach(() => {
+  });
+
+  it('shallow renders', () => {
+    shallow(<GameBoard store={store} />);
+  });
+});
