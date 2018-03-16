39fd58c839edc49ca5f3a5930f19a42d13dafe24
Grant Reed <grant.davis.reed@gmail.com>
2018-03-16 07:47:20 -0700

    Collisions (#31)
    * working collision detection
    
    * refactor collision function
    
    * use constants for snake status
    
    * collision changes snake status
    
    * peers receive and update snake status

--- client/src/constants.js
+++ client/src/constants.js
@@ -6,8 +6,11 @@ export const INITIAL_TU = 0;
 // number of TUs retained for snakes and boards
 // beyond what is currently displayed (according to snake length)
 export const HISTORY_LENGTH = 10;
+export const NUMBER_CANDIDATE_TUS = 3;
 
 export const INITIAL_SNAKE_LENGTH = 4;
+export const SNAKE_STATUS_ALIVE = 'alive';
+export const SNAKE_STATUS_DEAD = 'dead';
 
 export const GAME_STATUS_LOBBY = 'lobby'; // peer connections
 export const GAME_STATUS_PREGAME = 'pregame'; // no new peers, share initial positions
--- client/src/redux/snake/snakeActionCreators.js
+++ client/src/redux/snake/snakeActionCreators.js
@@ -3,6 +3,7 @@ import * as actionTypes from '../actionTypes';
 import * as snakeHelpers from './snakeHelpers';
 import * as p2pActions from '../p2p/p2pActionCreators';
 import * as helpers from '../metaHelpers';
+import * as constants from '../../constants';
 
 export const changeSnakeDirection = (id, direction) => ({
   id,
@@ -58,6 +59,65 @@ export const writeOwnSnakePosition = id => (
 
     dispatch(updateSnakeData(id, newSnake));
     p2pActions.p2pBroadcastSnakeData();
+  }
+);
+
+export const coordinatesMatch = (coordsA, coordsB) => (
+  coordsA.row === coordsB.row && coordsA.column === coordsB.column
+);
+
+export const isNotOwnHead = (tu1, tu2, id1, id2) => (
+  Number(tu1) !== Number(tu2) || id1 !== id2
+);
+
+export const getCollisionType = (headCoords, myID, peerID, snakeLength) => {
+  const peerSnake = store.getState().snakes[peerID];
+  const peerHeadTU = peerSnake.positions.byIndex[0];
+  const peerHeadCoords = peerSnake.positions.byKey[peerHeadTU];
+  const peerTailTU = peerSnake.positions.byIndex[snakeLength - 1];
+  const peerTailCoords = peerSnake.positions.byKey[peerTailTU - 1];
+  if (myID !== peerID && coordinatesMatch(headCoords, peerHeadCoords)) {
+    return 'HEAD-ON-HEAD COLLISION';
+  } else if (coordinatesMatch(headCoords, peerTailCoords)) {
+    return 'HEAD-ON-TAIL COLLISION';
+  }
+
+  return 'HEAD-ON-BODY COLLISION';
+};
+
+export const checkForCollisions = id => (
+  (dispatch) => {
+    const snakes = store.getState().snakes;
+    const mySnake = snakes[id];
+    const myLastTU = Number(mySnake.positions.byIndex[0]);
+
+    // candidate tus are myLastTU to myLastTU - NUMBER_CANDIDATE_TUS + 1
+    let headTUCounter = myLastTU;
+    while (headTUCounter > myLastTU - constants.NUMBER_CANDIDATE_TUS) {
+      const myHeadCoordsAtTU = mySnake.positions.byKey[headTUCounter];
+      const snakeLength = snakeHelpers.getSnakeLength(headTUCounter);
+      const snakeIDs = Object.keys(snakes);
+      for (let i = 0; i < snakeIDs.length; i += 1) {
+        const snakeID = snakeIDs[i];
+        const snake = snakes[snakeID];
+        // range is headTUCounter to headTUCounter - snakeLength + 1
+        let counter = headTUCounter;
+        while (counter > headTUCounter - snakeLength) {
+          const snakeCoordsAtTU = snake.positions.byKey[counter];
+          if (snakeCoordsAtTU &&
+              isNotOwnHead(counter, headTUCounter, id, snakeID)) {
+            if (coordinatesMatch(myHeadCoordsAtTU, snakeCoordsAtTU)) {
+              dispatch(changeSnakeStatus(id, constants.SNAKE_STATUS_DEAD));
+              console.log(getCollisionType(myHeadCoordsAtTU, id, snakeID, snakeLength));
+            }
+          }
+
+          counter -= 1;
+        }
+      }
+
+      headTUCounter -= 1;
+    }
   }
 );
 
