3c6daa9cb80853508cf0aaa4faeeb691ef020c19
Sienna M. Wood <siennamw@users.noreply.github.com>
2018-03-15 22:52:46 -0600

    Usernames (#30)
    * form for entering username
    
    * actions to store username in redux
    
    * actions to broadcast and receive usernames
    
    * connect react components for username actions
    
    * remove unneeded import
    
    * ignore blank and default usernames

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -4,37 +4,75 @@ import propTypes from 'prop-types';
 import * as constants from '../constants';
 
 export default class Lobby extends React.Component {
+  state = {
+    value: '',
+    showForm: true,
+  };
+
   static propTypes = {
-    ownPeerId: propTypes.string,
     changeGameStatus: propTypes.func,
+    ownPeerId: propTypes.string,
+    setOwnUsername: propTypes.func,
+    username: propTypes.string,
   };
 
   static defaultProps = {
+    changeGameStatus: () => {},
     ownPeerId: '',
-    changeGameStatus: () => {
-    },
+    setOwnUsername: () => {},
+    username: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
   };
 
   handlePlayClick = () => {
     this.props.changeGameStatus(constants.GAME_STATUS_PREGAME);
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ showForm: false });
+    console.log('username', this.state.value);
+    this.props.setOwnUsername(this.state.value);
+  };
+
   render() {
+    let content = '';
+    if (this.state.showForm) {
+      content = (
+        <form onSubmit={this.handleSubmit}>
+          <label className='label' htmlFor='username'>
+            Your name: <input type='text' name='username'
+                              onChange={this.handleChange} />
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
+      );
+    } else {
+      content = (
+        <div>
+          <div id='messages'>
+            <p>Sharing link: <a
+              href={`${window.location.origin}/${this.props.ownPeerId}`}>
+              {`${window.location.origin}/${this.props.ownPeerId}`}
+            </a>
+            </p>
+          </div>
+          <input
+            type='button'
+            value='Play with Connected Players'
+            onClick={this.handlePlayClick}
+          />
+        </div>
+      );
+    }
+
     return (
       <div>
         <h1>Welcome</h1>
-        <div id='messages'>
-          <p>Sharing link: <a
-            href={`${window.location.origin}/${this.props.ownPeerId}`}>
-            {`${window.location.origin}/${this.props.ownPeerId}`}
-          </a>
-          </p>
-        </div>
-        <input
-          type='button'
-          value='Play with Connected Players'
-          onClick={this.handlePlayClick}
-        />
+        {content}
       </div>
     );
   }
