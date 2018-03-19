731cf97ae5a10a449870c8c244a06bb4e8a5d570
Sienna <sienna.m.wood@gmail.com>
2018-03-19 10:10:30 -0600

    remove starting row function from metaHelpers

--- client/src/redux/metaHelpers.js
+++ client/src/redux/metaHelpers.js
@@ -1,18 +1,3 @@
-import random from 'lodash/random';
-import * as constants from '../constants';
-
 export const deepClone = obj => (
   JSON.parse(JSON.stringify(obj))
 );
-
-const rowsUsed = [];
-
-export const randomUniqueRow = () => {
-  let row = random(0, constants.GRID_SIZE - 1);
-  while (rowsUsed.includes(row)) {
-    row = random(0, constants.GRID_SIZE - 1);
-  }
-
-  rowsUsed.push(row);
-  return row;
-};
