c2dd0d980b49e525072043e6195d870b11f0b2fa
Sienna <sienna.m.wood@gmail.com>
2018-03-20 13:22:52 -0600

    function to declare game over

--- client/src/redux/metaActionCreators.js
+++ client/src/redux/metaActionCreators.js
@@ -7,6 +7,7 @@ import * as infoActions from './info/infoActionCreators';
 import * as p2pActions from './p2p/p2pActionCreators';
 import * as snakeActions from './snake/snakeActionCreators';
 import * as snakeHelpers from './snake/snakeHelpers';
+import * as p2pHelpers from './p2p/p2pHelpers';
 
 export const handleTuTick = id => (
   (dispatch) => {
@@ -54,5 +55,17 @@ export const resetGameData = () => (
     dispatch(boardActions.resetBoard());
     dispatch(headSetActions.resetHeadSets());
     dispatch(infoActions.setTu(0));
+    dispatch(infoActions.resetWinner());
+  }
+);
+
+export const declareGameOver = peerId => (
+  (dispatch) => {
+    if (peerId) {
+      p2pActions.p2pBroadcastWinnerId(peerId);
+      const username = p2pHelpers.getUsername(peerId);
+      dispatch(infoActions.updateWinner(username));
+    }
+    dispatch(p2pActions.p2pBroadcastGameStatus(constants.GAME_STATUS_POSTGAME));
   }
 );
