6977721ce70aa6c7e36428a2bbc479b77a3f9e71
Sienna <sienna.m.wood@gmail.com>
2018-03-27 13:22:10 -0600

    tests for head set action creators

---
+++ client/src/__test__/headSetActionCreators.test.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../redux/actionTypes';
+import * as headSetActions from '../redux/headSet/headSetActionCreators';
+
+describe('Head set action creators', () => {
+  it('updateHeadSets returns expected object', () => {
+    const id = 'aeirgjfn5678';
+    const snake = {};
+    const gap = 7;
+
+    const obj = headSetActions.updateHeadSets(id, snake, gap);
+    expect(obj).toEqual({
+      id,
+      snake,
+      gap,
+      type: actionTypes.UPDATE_HEAD_SETS,
+    });
+  });
+
+  it('patchHeadSet returns expected object', () => {
+    const tu = 67;
+    const sqNum = 234;
+    const id = 'cvoibu678';
+
+    const obj = headSetActions.patchHeadSet(tu, sqNum, id);
+    expect(obj).toEqual({
+      tu,
+      sqNum,
+      id,
+      type: actionTypes.PATCH_HEAD_SET,
+    });
+  });
+
+  it('resetHeadSets returns expected object', () => {
+    const obj = headSetActions.resetHeadSets();
+    expect(obj).toEqual({ type: actionTypes.RESET_HEAD_SETS });
+  });
+});
