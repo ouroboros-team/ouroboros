21ed0dcbab30a25305708f65c05db219769e0a0c
Sienna <sienna.m.wood@gmail.com>
2018-03-29 20:39:53 -0600

    tests for some snake action creator thunks

--- client/src/__test__/snakeActionCreators.test.js
+++ client/src/__test__/snakeActionCreators.test.js
@@ -1,5 +1,12 @@
 import * as actionTypes from '../redux/actionTypes';
+import * as constants from '../constants';
+
 import * as snakeActions from '../redux/snake/snakeActionCreators';
+import * as p2pActions from '../redux/p2p/p2pActionCreators';
+
+import * as p2pHelpers from '../redux/p2p/p2pHelpers';
+import * as snakeHelpers from '../redux/snake/snakeHelpers';
+import * as metaActions from '../redux/metaActionCreators';
 
 describe('Snake action creators', () => {
   it('changeSnakeDirection returns expected object', () => {
@@ -41,5 +48,154 @@ describe('Snake action creators', () => {
   it('resetSnakeData returns expected object', () => {
     const obj = snakeActions.resetSnakeData();
     expect(obj).toEqual({ type: actionTypes.RESET_SNAKE_DATA });
+  });
+
+  describe('handleChangeSnakeStatus thunk', () => {
+    let dispatchSpy;
+
+    beforeEach(() => {
+      dispatchSpy = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('returns a function', () => {
+      expect(typeof snakeActions.handleChangeSnakeStatus()).toBe('function');
+    });
+
+    it('calls dispatch with changeSnakeStatus with passed id and status', () => {
+      const id = 'sbihou38457';
+      const data = {};
+
+      snakeActions.handleChangeSnakeStatus(id, data)(dispatchSpy);
+      expect(dispatchSpy.mock.calls[0]).toEqual([
+        snakeActions.changeSnakeStatus(id, data),
+      ]);
+    });
+
+    // Problems testing thunks calling thunks
+    //
+    // it('calls dispatch with checkForGameOver', () => {
+    //   const id = 'sbihou38457';
+    //   const data = {};
+    //
+    //   snakeActions.handleChangeSnakeStatus(id, data)(dispatchSpy);
+    //   expect(dispatchSpy.mock.calls[1]).toEqual([
+    //     snakeActions.checkForGameOver()
+    //   ]);
+    // });
+
+    it('calls p2pActions.p2pBroadcastSnakeData if passed id is own id', () => {
+      const id = 'sbihou38457';
+      jest.spyOn(p2pHelpers, 'getOwnId').mockImplementation(() => (id));
+      const spy = jest.spyOn(p2pActions, 'p2pBroadcastSnakeData').mockImplementation(() => {});
+
+      snakeActions.handleChangeSnakeStatus(id, {})(dispatchSpy);
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('handleChangeSnakeDirection thunk', () => {
+    let dispatchSpy;
+
+    beforeEach(() => {
+      dispatchSpy = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('returns a function', () => {
+      expect(typeof snakeActions.handleChangeSnakeDirection()).toBe('function');
+    });
+
+    it('calls snakeHelpers.snakeIsAlive with passed id', () => {
+      const id = 'sbihou38457';
+      const direction = 'lejrb';
+      const spy = jest.spyOn(snakeHelpers, 'snakeIsAlive').mockImplementation(() => (true));
+      snakeActions.handleChangeSnakeDirection(id, direction)(dispatchSpy);
+      expect(spy).toHaveBeenCalledWith(id);
+    });
+
+    it('calls dispatch with changeSnakeDirection if snake is alive', () => {
+      const id = 'sbihou38457';
+      const direction = 'lejrb';
+      jest.spyOn(snakeHelpers, 'snakeIsAlive').mockImplementation(() => (true));
+
+      snakeActions.handleChangeSnakeDirection(id, direction)(dispatchSpy);
+      expect(dispatchSpy).toHaveBeenCalledWith(snakeActions.changeSnakeDirection(id, direction));
+    });
+
+    it('calls p2pActions.p2pBroadcastSnakeData if snake is alive', () => {
+      const id = 'sbihou38457';
+      const direction = 'lejrb';
+      jest.spyOn(snakeHelpers, 'snakeIsAlive').mockImplementation(() => (true));
+      const broadcastSpy = jest.spyOn(p2pActions, 'p2pBroadcastSnakeData').mockImplementation(() => {});
+
+      snakeActions.handleChangeSnakeDirection(id, direction)(dispatchSpy);
+      expect(broadcastSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('handleUpdateSnakeData thunk', () => {
+    let dispatchSpy;
+
+    beforeEach(() => {
+      dispatchSpy = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('returns a function', () => {
+      expect(typeof snakeActions.handleUpdateSnakeData()).toBe('function');
+    });
+
+    // Problems testing thunks calling thunks
+    //
+    // it('calls handleChangeSnakeStatus if passed snake data shows status of dead', () => {
+    //   const id = 'sbihou38457';
+    //   const data = { status: constants.SNAKE_STATUS_DEAD };
+    //
+    //   snakeActions.handleUpdateSnakeData(id, data)(dispatchSpy);
+    //   expect(dispatchSpy.mock.calls[0]).toEqual([
+    //     snakeActions.handleChangeSnakeStatus()
+    //   ]);
+    // });
+
+    it('calls updateSnakeData with passed id and data', () => {
+      const id = 'sbihou38457';
+      const data = {};
+
+      snakeActions.handleUpdateSnakeData(id, data)(dispatchSpy);
+      expect(dispatchSpy.mock.calls[0]).toEqual([
+        snakeActions.updateSnakeData(id, data),
+      ]);
+    });
+  });
+
+  describe('initializeOwnSnake thunk', () => {
+  });
+
+  describe('writeOwnSnakePosition thunk', () => {
+  });
+
+  describe('getCollisionType thunk', () => {
+  });
+
+  describe('checkForCollisions thunk', () => {
+  });
+
+  describe('checkForGameOver thunk', () => {
+  });
+
+  describe('checkForLatentSnakes thunk', () => {
   });
 });
