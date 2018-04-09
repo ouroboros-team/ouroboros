639cba1d8d5294739616123ed71a71071f275769
Sienna <sienna.m.wood@gmail.com>
2018-04-09 14:43:07 -0600

    fix dead snake visibility bug

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -4,29 +4,35 @@ import { connect } from 'react-redux';
 
 import Square from './Square';
 import { GRID_SIZE } from '../constants';
+import { snakeIsAlive } from '../redux/snake/snakeHelpers';
 
 const GameBoard = (props) => {
   let status;
   let styleId;
   const squares = [];
+  const statuses = {};
 
   for (let n = 0; n < GRID_SIZE * GRID_SIZE; n++) {
-      if (props.board[n] && props.board[n].snake) {
-        status = !props.board[n].snake.tuOfDeath ? 'alive' : 'dead';
-        styleId = props.board[n].snake.styleId;
-      } else {
-        status = 'empty';
-        styleId = undefined;
+    if (props.board[n] && props.board[n].snake) {
+      // memoize snake statuses
+      if (!statuses[props.board[n].id]) {
+        statuses[props.board[n].id] = snakeIsAlive(props.board[n].id) ? 'alive' : 'dead';
       }
+      status = statuses[props.board[n].id];
+      styleId = props.board[n].snake.styleId;
+    } else {
+      status = 'empty';
+      styleId = undefined;
+    }
 
-      squares.push(
-        <Square
-          key={n}
-          number={n}
-          status={status}
-          styleId={styleId}
-        />,
-      );
+    squares.push(
+      <Square
+        key={n}
+        number={n}
+        status={status}
+        styleId={styleId}
+      />,
+    );
   }
 
   return (
