09055a6dc57b3084c476a340d196fa29ec824596
Sienna <sienna.m.wood@gmail.com>
2018-04-12 14:59:48 -0600

    update FilterLink

--- client/src/components/FilterLink.js
+++ client/src/components/FilterLink.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const FilterLink = ({ filter, children, className }) => (
-  <NavLink to={filter === 'home' ? '/' : `/${filter}`} className={className}>
+const FilterLink = ({ filter, children, ...other }) => (
+  <NavLink {...other} exact to={filter === 'home' ? '/' : `/${filter}`}>
     {children}
   </NavLink>
 );
