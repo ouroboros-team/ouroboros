23c251eff919bfcdc7632015d4a8dc0e35bb99f8
Sienna M. Wood <siennamw@users.noreply.github.com>
2018-04-14 13:18:44 -0600

    code clean up (#80)
    * code clean up
    
    * add excludeHead to aggregateOwnSnake and use for collision checking
    
    * fix styling on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,22 +5,24 @@ import gameBoard from '../assets/images/partial-game-board.png';
 
 const HowToPlay = () => (
   <main className='container'>
-    <div className='eight columns'>
-      <h1>Welcome</h1>
-      <p>Ouroboros is a peer-to-peer snake game built
-        by <a href='http://siennamwood.com/'>Sienna M. Wood</a> and Grant
-        Reed to explore peer-to-peer gaming architectures and their challenges.
-      </p>
-      <ul>
-        <li><FilterLink filter='play'>Play the game</FilterLink></li>
-        <li>More about this project (coming soon!)</li>
-        <li><FilterLink filter='how-to-play'>How to play</FilterLink></li>
-        <li>Problems? <FilterLink filter='troubleshooting'>Get help</FilterLink></li>
-        <li><a href='https://github.com/ouroboros-team/ouroboros'>Check out our code on GitHub</a></li>
-      </ul>
-    </div>
-    <div className='four columns'>
-      <img alt='game board' src={gameBoard} />
+    <h1>Welcome</h1>
+    <div className='row'>
+      <div className='seven columns'>
+        <p>Ouroboros is a peer-to-peer snake game built
+          by <a href='http://siennamwood.com/'>Sienna M. Wood</a> and Grant
+          Reed to explore peer-to-peer gaming architectures and their challenges.
+        </p>
+        <ul>
+          <li><FilterLink filter='play'>Play the game</FilterLink></li>
+          <li>More about this project (coming soon!)</li>
+          <li><FilterLink filter='how-to-play'>How to play</FilterLink></li>
+          <li>Problems? <FilterLink filter='troubleshooting'>Get help</FilterLink></li>
+          <li><a href='https://github.com/ouroboros-team/ouroboros'>Check out our code on GitHub</a></li>
+        </ul>
+      </div>
+      <div className='five columns align-center'>
+        <img alt='game board' src={gameBoard} />
+      </div>
     </div>
   </main>
 );
